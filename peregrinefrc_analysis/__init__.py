02509c0b98eeda85a47bf3070ce4d813c4deb524
Chandler <Chandlerbrett@outlook.com>
2026-02-02 17:29:21 -0800

    Added the rest if analysis

--- peregrinefrc_analysis/__init__.py
+++ peregrinefrc_analysis/__init__.py
@@ -2,7 +2,3 @@ from peregrine_client import PeregrineClient
 
 __version__ = "0.2.0"
 __all__ = ["PeregrineClient"]
-
-event = "2025wass"
-username = "Alava"
-password = "heyisaac"
--- peregrinefrc_analysis/analysis_2023.py
+++ peregrinefrc_analysis/analysis_2023.py
@@ -26,16 +26,37 @@ def L4_auto(entry_data: dict) -> bool:
 
 def deep_atempt(entry_data: dict) -> bool:
     return "Deep Climb Attempt" in entry_data["name"]
+def Algae_proc_auto(entry_data: dict) -> bool:
+    return "Algae in Processor (auto)" in entry_data["name"]
+def Algae_in_net_auto(entry_data: dict) -> bool:
+    return "Algae in Net (auto)" in entry_data["name"]
+def Algae_proc_tele(entry_data: dict) -> bool:
+    return "Algae in Processor (teleop)" in entry_data["name"]
+def Robot_died(entry_data: dict) -> bool:
+    return "Robot Died" in entry_data["name"]
+def Played_defense(entry_data: dict) -> bool:
+    return "Played Defense" in entry_data["name"]
+def Shallow_climb_atempt(entry_data: dict) -> bool:
+    return "Shallow Climb Attempt" in entry_data["name"]
+def Algae_in_net_tele(entry_data: dict) -> bool:
+    return "Algae in Net (teleop)" in entry_data["name"]
 COUNT_FUNCTIONS = [
     L1_auto,
     L2_auto,
     L3_auto,
     L4_auto,
+    Algae_proc_auto,
+    Algae_in_net_auto,
     L1_tele,
     L2_tele,
     L3_tele,
     L4_tele,
-    deep_atempt
+    Algae_proc_tele,
+    Algae_in_net_tele,
+    Shallow_climb_atempt,
+    deep_atempt,
+    Played_defense,
+    Robot_died
     
 
 ]
@@ -44,9 +65,16 @@ COUNT_NAMES = [
     "Coral L2 (auto)",
     "Coral L3 (auto)",
     "Coral L4 (auto)",
+    "Algae in Processor (auto)",
+    "Algea in Net (auto)",
     "Coral L1 (teleop)",
     "Coral L2 (teleop)",
     "Coral L3 (teleop)",
     "Coral L4 (teleop)",
-    "Deep Climb Attempt"
+    "Algae in Processor (teleop)",
+    "Algea in Net (teleop)",
+    "Shallow Climb Attempt",
+    "deep_atempt",
+    "Played Defense",
+    "Robot Died"
 ]
